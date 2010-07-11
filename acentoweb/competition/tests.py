b39c4c9b395d729859886b804da95b6170494385
Israel Saeta Pérez <dukebody@gmail.com>
2010-07-11 17:36:43 +0200

    Refactor tests boilerplate - last one wasn't working properly.

--- acentoweb/competition/tests.py
+++ acentoweb/competition/tests.py
@@ -4,59 +4,33 @@ import unittest
 #from zope.component import testing
 from Testing import ZopeTestCase as ztc
 
-from Products.Five import fiveconfigure
+from Products.Five import fiveconfigure, zcml
 from Products.PloneTestCase import PloneTestCase as ptc
-from Products.PloneTestCase.layer import PloneSite
-ptc.setupPloneSite()
-
-import acentoweb.competition
+from Products.PloneTestCase.layer import onsetup
 
 
-class TestCase(ptc.PloneTestCase):
-
-    class layer(PloneSite):
-
-        @classmethod
-        def setUp(cls):
+@onsetup
+def setup_acentoweb_competition():
             fiveconfigure.debug_mode = True
-            ztc.installPackage(acentoweb.competition)
+            import acentoweb.competition
+            zcml.load_config('configure.zcml', acentoweb.competition)
             fiveconfigure.debug_mode = False
 
-        @classmethod
-        def tearDown(cls):
-            pass
+            ztc.installPackage('acentoweb.competition')
 
+setup_acentoweb_competition()
 ptc.setupPloneSite(products=['acentoweb.competition'])
 
-class TestWorkflow(TestCase):
+
+class TestWorkflow(ptc.PloneTestCase):
     def test_wf_exists(self):
         wt = self.portal.portal_workflow
-        self.failUnless('competition_workflow' in wt.objectIds())
+        self.failUnless('competition_item_workflow' in wt.objectIds())
 
 
 def test_suite():
     return unittest.TestSuite([
-
-        # Unit tests
-        #doctestunit.DocFileSuite(
-        #    'README.txt', package='acentoweb.competition',
-        #    setUp=testing.setUp, tearDown=testing.tearDown),
-
-        #doctestunit.DocTestSuite(
-        #    module='acentoweb.competition.mymodule',
-        #    setUp=testing.setUp, tearDown=testing.tearDown),
-
             unittest.makeSuite(TestWorkflow)
-
-        # Integration tests that use PloneTestCase
-        #ztc.ZopeDocFileSuite(
-        #    'README.txt', package='acentoweb.competition',
-        #    test_class=TestCase),
-
-        #ztc.FunctionalDocFileSuite(
-        #    'browser.txt', package='acentoweb.competition',
-        #    test_class=TestCase),
-
         ])
 
 if __name__ == '__main__':
