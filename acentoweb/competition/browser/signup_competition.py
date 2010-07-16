3c73e24fbd51bf31f20b1bb42da1edc7db60ed05
Israel Saeta Pérez <dukebody@gmail.com>
2010-07-16 12:27:00 +0200

    Add a signup button for competitions.

---
+++ acentoweb/competition/browser/signup_competition.py
@@ -0,0 +1,36 @@
+from AccessControl import Unauthorized
+from Products.Five.browser import BrowserView
+
+from Products.CMFPlone import PloneMessageFactory as _
+from Products.CMFCore.utils  import getToolByName
+
+
+class SignUp(BrowserView):
+    def __call__(self):
+
+        form = self.request.form
+        signup_button = form.get('form.button.signup', None) is not None
+
+        if signup_button:
+
+            # security checks
+            if not self.request.get('REQUEST_METHOD','GET') == 'POST':
+                raise Unauthorized
+
+            authenticator = self.context.restrictedTraverse('@@authenticator', None) 
+            if not authenticator.verify():
+                raise Unauthorized
+
+            mt = getToolByName(self.context, 'portal_membership')
+            member = mt.getAuthenticatedMember()
+            userid = member.getUserId()
+
+            if 'Competitor' not in self.context.get_local_roles_for_userid(userid):
+                self.context.manage_setLocalRoles(userid, ['Competitor',])
+
+                self.context.plone_utils.addPortalMessage(_(u"You've just signed up in the competition!"))
+
+            else:
+                self.context.plone_utils.addPortalMessage(_(u"You've already signed up in the competition!"))
+
+        return self.request.RESPONSE.redirect(self.context.absolute_url())
