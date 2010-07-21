12df9bb36fe779b9412f5e0614e36b79a439aa3c
Israel Saeta Pérez <dukebody@gmail.com>
2010-07-21 16:19:41 +0200

    Restrict the sharing page.

--- acentoweb/competition/permissions.py
+++ acentoweb/competition/permissions.py
@@ -1,5 +1,6 @@
 # CMF and Archetypes imports
 from Products.Archetypes.atapi import listTypes
+from Products.CMFCore.permissions import setDefaultRoles
 
 # Product imports
 from config import PROJECTNAME
@@ -11,3 +12,8 @@ types = listTypes(PROJECTNAME)
 for atype in  types:
     permission = "%s: Add %s" % (PROJECTNAME, atype['portal_type'])
     ADD_CONTENT_PERMISSIONS[atype['portal_type']] = permission
+
+
+# Controls access to the "sharing" page
+DelegateRoles = "Sharing page: Delegate roles"
+setDefaultRoles(DelegateRoles, ('Manager', 'Editor', 'Reviewer',))
